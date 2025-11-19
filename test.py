657181690e2968b56ddffa1f26ffa9890fe560e6
alinac1888 <alinac44@nycstudents.net>
2025-11-19 06:25:08 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 def login(email, password):
-    if email not in '@':
-        return "Not a valid email"
-    if len(password) != 8 and int(password, base = 0) not in password:
+    if not isinstance(email, str) or not isinstance(password, str):
+         return "Error: email and password are not strings"
+    if '@' not in email:
+            print('invalid')
+            return "invalid email"
+    if len(password) < 8:
+         return "invalid password"
+    if password.isdigit not in password:
         return "Not valid password"
-    if password is not password.capitalize:
-        return "not a valid password"
-    
-login("email@email.com", 'password')
+    for i in password:
+        if i.isupper in password:
+            print('valid')
+            return "a valid password"
+        else:
+            print('invalid')
+            return "Not valid password"
+login("email@email.email", "qwertyUiopasdfghjk1zxcvbnm")
+
 
