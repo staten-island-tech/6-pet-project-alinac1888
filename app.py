1ae2d743ec9afafa813733c97597a4fff7ed0fe9
alinac1888 <alinac44@nycstudents.net>
2025-11-24 06:26:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 class pet:
-    def __init__(self, name, money, inventory, hunger):
+    def __init__(self, name, money, inventory, happy):
         self.name = name
         self.money = money
         self.inventory = inventory
-        self.hunger = hunger
+        self.__happiness = happy
+        # self.hunger = hunger
+        # hunger = 100
+        happy = 100
 
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
     
-    def starvation(self, eat):
-        self.hunger += eat
-        print(f"{self.name} has ")
+    # def feed(self, food):
+    #     food = 20
+    #     self.hunger += food
+    #     print(f"{self.name}'s hunger is at {}")
+
+    def play(self, item):
+        item = 10
+        happy += item
+
+        
 
 usagi = pet("usagi", 2000000000000000, ["cheeseburger, hamburger, big mac, whopper, pibbl"], 1000)
 print(usagi.__dict__)
 
 usagi.buy({"title": "rod", "atk": 6000})
-
+usagi.play({""})
 
