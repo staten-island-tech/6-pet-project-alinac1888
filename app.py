9a9134d320ce89eac6b92ff5d8bda5abefcb7d51
alinac1888 <alinac44@nycstudents.net>
2025-11-26 06:26:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,31 +5,37 @@ class pet:
         self.inventory = inventory
         self.__happiness = happy
         self.hunger = hunger
+        happy = 100
+        hunger = 100
+
+    food = ["cheeseburger", "hamburger", "big mac", "whopper", "pibbl"]
 
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
-    
 
-    def starve(self, item):
+    def starve(self, item, hunger):
         item = 1
         hunger -= item
         if hunger < 50:
             print(f"{self.name} is hungry")
             happiness -= item
 
-    def feed(self, food):
+    def feed(self, food, hunger):
         food = 20
-        hunger += food
-        if hunger >= 50 and hunger <= 100:
+        self.hunger += food
+        if self.hunger >= 50 and hunger <= 100:
             print(f"{self.name} is not hungry")
-        if hunger < 50 and hunger >= 0:
+        if self.hunger < 50 and hunger >= 0:
             print(f"{self.name} is hungry")
+        if self.hunger > 100:
+            print(f"{self.name} is overfed")
+        print(self.hunger)
 
+    
 
-usagi = pet("usagi", 2000000000000000, ["cheeseburger, hamburger, big mac, whopper, pibbl"], 1000, 100)
+usagi = pet("usagi", 2000000000000000, ["cheeseburger, hamburger, big mac, whopper, pibbl"], 100, 100)
 print(usagi.__dict__)
 
-usagi.buy({"title": "rod", "atk": 6000})
-usagi.feed(food: "")
+usagi.feed("hamburger")
 
