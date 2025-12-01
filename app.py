f3f0244d2aa1da68c7c4b1ea17db29c535cd0ed5
alinac1888 <alinac44@nycstudents.net>
2025-11-30 19:10:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,38 +3,50 @@ class pet:
         self.name = name
         self.money = money
         self.inventory = inventory
-        self.__happiness = happy
+        self.happiness = happy
         self.hunger = hunger
         happy = 100
         hunger = 100
-
-    food = ["cheeseburger", "hamburger", "big mac", "whopper", "pibbl"]
+        self.living = True
 
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
 
-    def starve(self, item, hunger):
-        item = 1
-        hunger -= item
+    def maybe_boredom(self, item):
+        while self.living == True:
+            if self.happiness >= 50 and self.happiness <= 100:
+                print(f"{self.name} is content, happiness is at {self.hunger}")
+            if self.hunger < 50 and self.hunger >= 0:
+                print(f"{self.name} is hungry, hunger is at {self.hunger}")
+                happiness -=
+            if self.hunger > 100 and self.hunger:
+                hunger = 100
+                print(f"{self.name} is overfed, hunger is at {self.hunger}")
+    def starve(self, hunger):
+
         if hunger < 50:
             print(f"{self.name} is hungry")
             happiness -= item
 
-    def feed(self, food, hunger):
-        food = 20
-        self.hunger += food
-        if self.hunger >= 50 and hunger <= 100:
-            print(f"{self.name} is not hungry")
-        if self.hunger < 50 and hunger >= 0:
-            print(f"{self.name} is hungry")
-        if self.hunger > 100:
-            print(f"{self.name} is overfed")
-        print(self.hunger)
+    def feed(self, food, ):
+        while self.living == True:
+
+            if self.hunger >= 50 and self.hunger <= 100:
+                print(f"{self.name} is not hungry, hunger is at {self.hunger}")
+            if self.hunger < 50 and self.hunger >= 0:
+                print(f"{self.name} is hungry, hunger is at {self.hunger}")
+                happiness -=
+            if self.hunger > 100 and self.hunger:
+                hunger = 100
+                print(f"{self.name} is overfed, hunger is at {self.hunger}")
+            else:
+                print(f"neglected {self.name}")
+                self.living = False
 
     
 
-usagi = pet("usagi", 2000000000000000, ["cheeseburger, hamburger, big mac, whopper, pibbl"], 100, 100)
+usagi = pet("usagi", 2000000000000000, ["cheeseburger, hamburger, big mac, whopper, pibbl"])
 print(usagi.__dict__)
 
 usagi.feed("hamburger")
