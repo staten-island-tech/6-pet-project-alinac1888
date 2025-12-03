63ea75cae676c7d0d2336ac258a8b2f1f3af0c6b
alinac1888 <alinac44@nycstudents.net>
2025-12-03 06:26:08 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 class pet:
-    def __init__(self, name, money, inventory, happy, hunger):
+    def __init__(self, name, money, inventory, happy, hunger, living):
         self.name = name
         self.money = money
         self.inventory = inventory
@@ -7,14 +7,14 @@ class pet:
         self.hunger = hunger
         hunger = 50
         happy = 50
-        self.living = True
+        self.living = living
+        living = True
 
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
 
     def maybe_boredom(self):
-        while self.living == True:
             if self.happiness >= 50 and self.happiness <= 100:
                 print(f"{self.name} is content, happiness is at {self.happiness}")
             elif self.happiness < 50 and self.happiness >= 0:
@@ -27,41 +27,37 @@ class pet:
                 print(f"sad {self.name}")
                 self.living = False
 
-    def starve(self):
+    def happies(self, play):
         while self.living == True:
-            if self.hunger >= 50 and self.hunger <= 100:
-                print(f"{self.name} is not hungry, hunger is at {self.hunger}")
-            elif self.hunger < 50 and self.hunger >= 0:
+            
+
+    def starve(self):
+        if self.hunger >= 50 and self.hunger <= 100:
+            print(f"{self.name} is not hungry, hunger is at {self.hunger}")
+        elif self.hunger < 50 and self.hunger >= 0:
                 print(f"{self.name} is hungry, hunger is at {self.hunger}")
                 happiness -= 1
-            elif self.hunger > 100 and self.hunger <= 130:
-                self.hunger = 100
+        elif self.hunger > 100 and self.hunger <= 130:
                 print(f"{self.name} is overfed, hunger is at {self.hunger}")
-            elif self.hunger > 130:
-                print(f"{self.name} DEAD")
-                self.living = False
-            else:
-                print(f"neglected {self.name}")
-                self.living = False
+        elif self.hunger > 130:
+            print(f"{self.name} DEAD")
+            self.living = False
+        else:
+            print(f"neglected {self.name}")
+            self.living = False
 
     def feed(self, eat):
-        while self.living == True:
-            eat = input(f"Feed {self.name}")
-            if eat in self.inventory:
-                self.hunger += 10
-                print(f"{self.name} is now at {self.hunger}%")
-                break
-            else:
-                print(f"food not found in {self.name}'s inventory")
-        if self.living == False:
-            print(f"{self.name} is dead")
+        if eat in self.inventory:
+            self.hunger += 10
+            print(f"{self.name} is now at {self.hunger}/100 hunger")
+        else:
+            print(f"food not found in {self.name}'s inventory")
     
     def stats(self):
         print(self.__dict__)
-        print(f"hunger bar:{self.starve}")
 
-usagi = pet("usagi", 2000000000000000, ["cheeseburger", "hamburger", "big mac", "whopper", "pibbl", "lemons"], 50, 50, True)
+usagi = pet("usagi", 2000000000000000, ["cheeseburger", "hamburger", "big mac", "whopper", "pibbl",], 50, 50, True)
 
 usagi.stats()
-usagi.feed("lemons")
-
+game = input("what woulf u do wityh ur pet first")
+    
