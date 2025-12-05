5e07315fb805c41e7416ed92eeb394d3b490545b
alinac1888 <alinac44@nycstudents.net>
2025-12-05 06:25:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ class pet:
         self.inventory = inventory
         self.happiness = happy
         self.hunger = hunger
-        hunger = 50
-        happy = 50
+        hunger = 90
+        happy = 90
         self.living = living
         living = True
 
@@ -27,25 +27,25 @@ class pet:
                 print(f"sad {self.name}")
                 self.living = False   
 
-    def starve(self):
-        if self.hunger >= 50 and self.hunger <= 100:
-            print(f"{self.name} is not hungry, hunger is at {self.hunger}")
-        elif self.hunger < 50 and self.hunger >= 0:
-                print(f"{self.name} is hungry, hunger is at {self.hunger}")
-                happiness -= 1
-        elif self.hunger > 100 and self.hunger <= 130:
-                print(f"{self.name} is overfed, hunger is at {self.hunger}")
-        elif self.hunger > 130:
-            print(f"{self.name} DEAD")
-            self.living = False
-        else:
-            print(f"neglected {self.name}")
-            self.living = False
 
     def feed(self, eat):
         if eat in self.inventory:
             self.hunger += 10
+
             print(f"{self.name} is now at {self.hunger}/100 hunger")
+            if self.hunger >= 50 and self.hunger < 100:
+                print(f"{self.name} is not hungry")
+            elif self.hunger < 50 and self.hunger >= 0:
+                print(f"{self.name} is hungry")
+            elif self.hunger == 100:
+                print(f"{self.name} is full")
+            elif self.hunger > 100:
+                print(f"{self.name} DEAD")
+                self.living = False
+            else:
+                print(f"neglected {self.name}")
+                self.living = False
+                
         else:
             print(f"food not found in {self.name}'s inventory")
     
@@ -56,38 +56,31 @@ class pet:
         while self.living == True:
             if choice == "feed":
                 self.feed(eat = input("what to eat"))
-                upstats = input("Do you want current stats? Y/N")
-                if upstats == "Y":
-                        print(self.__dict__)
-                        self.gameshhssjsa(choice = input("What else do you want to do?"))
-                elif upstats == "N":
-                        self.gameshhssjsa(choice = input("What else do you want to do?"))
-                else:
-                     print("invalid option, please try again")
-                     self.gameshhssjsa(choice = input("What else do you want to do?"))
-                break
             elif choice == "buy":
                 self.buy(item = input("what to buy"))
-                upstats = input("Do you want current stats? Y/N")
-                if upstats == "Y":
-                    print(self.__dict__)
-                    self.gameshhssjsa(choice = input("What else do you want to do?")) 
-                elif upstats == "N":
-                    self.gameshhssjsa(choice = input("What else do you want to do?"))
-                else:
-                    print("invalid option, please try again")
-                    self.gameshhssjsa(choice = input("What else do you want to do?"))
-                break
             else:
                 print("invalid option, please try again")
                 self.gameshhssjsa(choice = input("what do you want to do"))
                 break
+            upstats = input("Do you want current stats? Y/N")
+            if upstats == "Y":
+                    print(self.__dict__)
+                    if self.living == True:
+                        self.gameshhssjsa(choice = input("What else do you want to do?"))
+            elif upstats == "N":
+                if self.living == True:
+                    self.gameshhssjsa(choice = input("What else do you want to do?"))
+            else:
+                print("invalid option, please try again")
+                if self.living == True:
+                        self.gameshhssjsa(choice = input("What else do you want to do?"))
+            break
         while self.living == False:
-             print("dead")
-             break
+            print("dead")
+            break
 
 
-usagi = pet("usagi", 2000000000000000, ["cheeseburger", "hamburger", "big mac", "whopper", "pibbl"], 50, 50, True)
+usagi = pet("usagi", 2000000000000000, ["cheeseburger", "hamburger", "big mac", "whopper", "pibbl"], 90, 90, True)
 
 usagi.stats()
 usagi.gameshhssjsa(choice = input("what do you want to do"))
